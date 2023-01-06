20a96fdb70caa6fa9e57c738d38e8ed95fe52b72
Felipe Leiva Cortés <felipe-leiva@hotmail.cl>
2023-01-06 13:03:55 -0300

    Add User

--- funcionarios.php
+++ funcionarios.php
@@ -11,9 +11,9 @@
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="scripts/funcionarios.js"></script>
-    <script src="https://github.com/FelipeLeivaCortes/Rut/PluginRut.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/FelipeLeivaCortes/Rut/PluginRut.js"></script>
 </head>
 <body>
     <div class="container">
--- scripts/funcionarios.js
+++ scripts/funcionarios.js
@@ -11,27 +11,27 @@ function register()
      * Checking all inputs required
      */
     if ( $('#rut').val() == '' ) {
-        swal('Error', 'Ingrese rut del funcionario', 'error');
+        Swal.fire('Error', 'Ingrese rut del funcionario', 'error');
         return;
     }
 
     if ( $('#name').val() == '' ) {
-        swal('Error', 'Ingrese nombre del funcionario', 'error');
+        Swal.fire('Error', 'Ingrese nombre del funcionario', 'error');
         return;
     }
 
     if ( $('#address').val() == '' ) {
-        swal('Error', 'Ingrese dirección del funcionario', 'error');
+        Swal.fire('Error', 'Ingrese dirección del funcionario', 'error');
         return;
     }
 
     if ( $('#birthday').val() == '' ) {
-        swal('Error', 'Ingrese fecha de nacimiento del funcionario', 'error');
+        Swal.fire('Error', 'Ingrese fecha de nacimiento del funcionario', 'error');
         return;
     }
 
     if ( $('#state').val() == null ) {
-        swal('Error', 'Seleccione estado del funcionario', 'error');
+        Swal.fire('Error', 'Seleccione estado del funcionario', 'error');
         return;
     }
 
@@ -57,17 +57,17 @@ function register()
 
     }).fail(function(error) {
         console.log(error);
-        swal('Error', 'Se ha producido un error con el servicio "register.php"', 'error');
+        Swal.fire('Error', 'Se ha producido un error con el servicio "register.php"', 'error');
     
     }).done(function(res) {
       //  console.clear();
         console.log(res);
 
         if ( res.error ) {
-            swal('Error', res.msg, 'error');
+            Swal.fire('Error', res.msg, 'error');
             return;
         }
 
-        swal('Operación Exitosa', res.msg, 'success');
+        Swal.fire('Operación Exitosa', res.msg, 'success');
     });
 }
